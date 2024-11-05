6efda64fabaf165b1513095483fb9b5a99a30ea9
leehah <leehah0908@naver.com>
2024-11-05 14:21:42 +0900

    Header, Footer, Home 추가

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Header />
+            <div className='content-wrapper'>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/member/create' element={<Home />} />
+                    <Route path='/login' element={<Home />} />
+                </Routes>
+            </div>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
