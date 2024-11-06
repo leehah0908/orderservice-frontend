d053cc3eadd3deabeae2a09747e49b1ba53e7880
leehah <leehah0908@naver.com>
2024-11-06 16:09:45 +0900

    마이페이지(회원) 기능 추가

--- src/App.js
+++ src/App.js
@@ -5,22 +5,31 @@ import Header from './components/Header';
 import Home from './components/Home';
 import Signup from './components/Signup';
 import Login from './components/Login';
+import ProductList from './components/ProductList';
+import OrderPage from './components/OrderPage';
+import Mypage from './components/Mypage';
 import { AuthContextProvider } from './context/UserContext';
+import { CartContextProvider } from './context/CartContext';
 
 function App() {
     return (
         <AuthContextProvider>
-            <div className='App'>
-                <Header />
-                <div className='content-wrapper'>
-                    <Routes>
-                        <Route path='/' element={<Home />} />
-                        <Route path='/member/create' element={<Signup />} />
-                        <Route path='/login' element={<Login />} />
-                    </Routes>
+            <CartContextProvider>
+                <div className='App'>
+                    <Header />
+                    <div className='content-wrapper'>
+                        <Routes>
+                            <Route path='/' element={<Home />} />
+                            <Route path='/member/create' element={<Signup />} />
+                            <Route path='/login' element={<Login />} />
+                            <Route path='/product/list' element={<ProductList />} />
+                            <Route path='/order/cart' element={<OrderPage />} />
+                            <Route path='/mypage' element={<Mypage />} />
+                        </Routes>
+                    </div>
+                    <Footer />
                 </div>
-                <Footer />
-            </div>
+            </CartContextProvider>
         </AuthContextProvider>
     );
 }
---
+++ src/components/Mypage.jsx
@@ -0,0 +1,69 @@
+import { Card, CardContent, CardHeader, Container, Grid, Table, TableBody, TableCell, TableRow } from '@mui/material';
+import axios from 'axios';
+import React, { useContext, useEffect, useState } from 'react';
+import AuthContext from '../context/UserContext';
+
+const Mypage = () => {
+    const { userRole } = useContext(AuthContext);
+    const [memberInfoList, setMemberInfoList] = useState([]);
+
+    const isAdmin = userRole === 'ADMIN';
+
+    useEffect(() => {
+        // 회원정보 불러오기
+        const fetchMemberInfo = async () => {
+            try {
+                // if (!isAdmin) {
+                //     const res = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/user/list`);
+                //     setMemberInfoList(res.data.result);
+                // } else {
+                const res = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/user/myinfo`, {
+                    headers: {
+                        Authorization: 'Bearer ' + localStorage.getItem('ACCESS_TOKEN'),
+                    },
+                });
+
+                setMemberInfoList([
+                    { key: '이름', value: res.data.result.nickname },
+                    { key: '이메일', value: res.data.result.email },
+                    { key: '도시', value: res.data.result.address?.city || '등록 전' },
+                    { key: '상세 주소', value: res.data.result.address?.street || '등록 전' },
+                    { key: '우편 번호', value: res.data.result.address?.zipCode || '등록 전' },
+                ]);
+
+                // }
+            } catch (e) {}
+        };
+
+        fetchMemberInfo();
+    }, []);
+
+    return (
+        <Container>
+            <Grid container justifyContent='center'>
+                <Grid item xs={12} md={8}>
+                    <Card>
+                        <CardHeader title='회원정보' style={{ textAlign: 'center' }} />
+                        <CardContent>
+                            <Table>
+                                <TableBody>
+                                    {memberInfoList.map((element, index) => (
+                                        <TableRow key={index}>
+                                            <TableCell>{element.key}</TableCell>
+                                            <TableCell>{element.value}</TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Grid>
+
+            {/* OrderListComponent */}
+            {/* <OrderListComponent isAdmin={userRole === 'ADMIN'} /> */}
+        </Container>
+    );
+};
+
+export default Mypage;
