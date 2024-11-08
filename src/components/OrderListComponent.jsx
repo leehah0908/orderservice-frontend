783fc8f176b240de61a89a0fb461e818934d10ce
leehah <leehah0908@naver.com>
2024-11-08 17:58:23 +0900

    주문 목록 에러 처리

--- src/components/OrderListComponent.jsx
+++ src/components/OrderListComponent.jsx
@@ -12,11 +12,15 @@ import {
     Typography,
 } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import axiosInstance from '../config/axios-config';
+import AuthContext from '../context/UserContext';
+import { useNavigate } from 'react-router-dom';
 
 const OrderListComponent = ({ isAdmin }) => {
     const [orderList, setOrderList] = useState([]);
+    const { onLogout } = useContext(AuthContext);
+    const navigate = useNavigate();
 
     useEffect(() => {
         const fetchOrders = async () => {
@@ -37,10 +41,19 @@ const OrderListComponent = ({ isAdmin }) => {
             await axiosInstance.patch(`${process.env.REACT_APP_API_BASE_URL}/order/${orderId}/cancel`);
 
             // 주문 취소를 백엔드로 보내고, 이상이 없으면 주문 목록을 다시 랜더링해야 함
-            setOrderList((prevList) =>
-                prevList.map((order) => (order.orderId === orderId ? { ...order, orderStatus: 'CANCELED' } : order)),
-            );
-        } catch (e) {}
+            setOrderList((prevList) => {
+                return prevList.map((order) => (order.orderId === orderId ? { ...order, orderStatus: 'CANCELED' } : order));
+            });
+        } catch (e) {
+            if (e.response.data?.statusMessage === 'EXPIRED_RT') {
+                alert('시간이 경과되어 재로그인이 필요합니다.');
+                onLogout();
+                navigate('/');
+            } else if (e.response.data.message === 'NO_LOGIN') {
+                alert('아예 로그인 X');
+                navigate('/');
+            }
+        }
     };
 
     return (
